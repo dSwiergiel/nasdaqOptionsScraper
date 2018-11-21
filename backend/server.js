2373cf2f28f2d2d54e0b819944c17d61d0fa1d3a
Deven Swiergiel <dswiergiel@albany.edu>
2018-11-21 18:06:14 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -149,8 +149,8 @@ router.route("/loginUser").post((req, res) => {
   );
 });
 app.get('/', function(req, res) {
-  // res.sendFile(path.join(__dirname + 'dist/index.html'));
-  res.send('THIS IS A TEST')
+  res.sendFile(path.join(__dirname + 'dist/index.html'));
+  // res.send('THIS IS A TEST')
 });
 app.use("/", router);
 // viewed at http://localhost:8080
